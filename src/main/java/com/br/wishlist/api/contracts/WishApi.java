3220e8a823f629f7b82accf3b252182c35266f71
robertorsm <sroberto.rsm@gmail.com>
2024-10-19 04:53:03 -0300

    refactor: adicionando request mapping.

--- src/main/java/com/br/wishlist/api/contracts/WishApi.java
+++ src/main/java/com/br/wishlist/api/contracts/WishApi.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/wish-list")
 public interface WishApi {
 
     @Operation(summary = "Get all wishes")
